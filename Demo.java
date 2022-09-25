adf93b383387f5499b3d6f69fa5df02ac8eb800f
Bhushan279 <113437779+Bhushan279@users.noreply.github.com>
2022-09-25 22:46:09 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,23 @@
+class Demo{
+Demo ()
+	{
+	 System.out.println("from demo()");
+	 
+	}
+Demo (int i)
+	{
+	System.out.println("int");
+	
+	}
+Demo (double b)
+	{
+	System.out.println("double");
+	}
+	
+	public static void main (String args [])
+	{
+		Demo obj1 = new Demo ();
+	
+	}
+	}
+	
---
+++ P3.java
@@ -0,0 +1,23 @@
+class P3{
+
+public static void main (String args[]){
+	for (int i=1;i<=6;i++)
+	{
+		for (int j=6;j>=i;j--)
+		{
+			System.out.print(" * ");
+		}
+		System.out.println();
+			
+	}
+	for (int i=1-1;i<=6;i++)
+	{
+		for (int j=1;j<=i;j++)
+		{
+			System.out.print(" * ");
+		}
+		System.out.println();
+	}
+	
+}
+}
